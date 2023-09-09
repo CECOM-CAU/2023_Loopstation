faa468628f04bbfa39fe07169daf8af2cb3bb1ea
naeun222 <135949580+naeun222@users.noreply.github.com>
2023-09-09 12:47:19 +0900

    Implement Neopixel

--- src/Neopixel.cpp
+++ src/Neopixel.cpp
@@ -1,10 +1,45 @@
 #include "Neopixel.h"
+#include <Adafruit_NeoPixel.h>
 
-Neopixel::Neopixel(int _pin, int _rgb, MetronomeModel* _model, unsigned long* _time){
+
+Neopixel::Neopixel(int _pin, int _rgb, MetronomeModel* _model, unsigned long* time){
     model = _model;
+    pin = _pin;
+    pixels = new Adafruit_NeoPixel(NUM_LEDS, _pin, NEO_GRBW + NEO_KHZ800);
+    pixels->begin();
+    pixels->show();
+    startTime = *time;
+    this->time = time;
+
 }
 
 void Neopixel::update(){
-    int r = model->getRhythm();
+
+    int bpm = model -> getTempo();
+    int rhythm = model -> getRhythm();
+    unsigned long tempo = 60000/static_cast<unsigned long>(bpm); //밀리세컨드로 변환한 템포
+
+    //여기까지 초기설정
+    //여기서부터 업데이트
+
+    int cycle = (*time - startTime)/tempo; //몇박째인지 (0박 시작)
+    int current = (*time - startTime)%tempo; //한 박 안에서 시간이 얼마나 지났는지
+
+    int i = current*16/tempo; //픽셀 위치
+
+    Serial.println(i);
+    if (cycle%rhythm == 0) { 
+        pixels->setPixelColor(i, Adafruit_NeoPixel::Color(255, 255, 255)); //리듬 첫박은 하얀색 빛
+        pixels->setBrightness(0xff);
+        pixels->show();
+    }
+    else {
+        pixels->setPixelColor(i, Adafruit_NeoPixel::Color(255, 0, 0)); //나머지 박은 빨간색 빛
+        pixels->setBrightness(0xff);
+        pixels->show();
+    }
+    pixels->setPixelColor((i-2+16)%16, Adafruit_NeoPixel::Color(0, 0, 0));
+    // pixels->setBrightness(0x0);
+    pixels->show();
 }
 
--- src/Neopixel.h
+++ src/Neopixel.h
@@ -1,10 +1,11 @@
+#ifndef __NEOPIXEL_H__
+#define __NEOPIXEL_H__
+
 #include "IUpdatable.h"
 #include "MetronomeModel.h"
 #include "Adafruit_NeoPixel.h"
 
-#define NEO_PIN   2      // 네오픽셀 DI핀과 연결된 핀번호 설정
 #define NUM_LEDS  16     // 네오픽셀 LED 갯수 설정
-
 
 class Neopixel : public IUpdatable{
 private:
@@ -14,6 +15,8 @@ private:
     
     MetronomeModel* model;
     unsigned long* time;
+    unsigned long startTime;
+    Adafruit_NeoPixel* pixels;
 
 
 public: 
@@ -21,53 +24,55 @@ public:
     void update();
 };
 
-    // 네오픽셀 라이브러리 funnyNeo 이름으로 연결
-    Adafruit_NeoPixel funnyNeo = Adafruit_NeoPixel(NUM_LEDS, NEO_PIN, NEO_GRBW + NEO_KHZ800);
+#endif
+
+//     // 네오픽셀 라이브러리 funnyNeo 이름으로 연결
+//     Adafruit_NeoPixel funnyNeo = Adafruit_NeoPixel(NUM_LEDS, NEO_PIN, NEO_GRBW + NEO_KHZ800);
 
 
-// 아두이노 초기화
-void setup() 
-{
-    funnyNeo.setBrightness(ledBright);  // 밝기 설정 ( 0 ~ 255 )
-    funnyNeo.begin();                   // 네오픽셀 시작
-    funnyNeo.show();                    // 네오픽셀 초기화
-}
+// // 아두이노 초기화
+// void setup() 
+// {
+//     funnyNeo.setBrightness(ledBright);  // 밝기 설정 ( 0 ~ 255 )
+//     funnyNeo.begin();                   // 네오픽셀 시작
+//     funnyNeo.show();                    // 네오픽셀 초기화
+// }
 
-void loop() 
-{
-    int i;
-    uint32_t colorValue;    // 색상값 저장 변수
+// void loop() 
+// {
+//     int i;
+//     uint32_t colorValue;    // 색상값 저장 변수
 
-    //빨간색 1바퀴 회전
-    colorValue = funnyNeo.Color(255, 0, 0, 0);      // Red
-    for(i = 0; i < NUM_LEDS; i++)
-    {
-        funnyNeo.setPixelColor(i, colorValue);
-        funnyNeo.show();
-        delay(50);
-    }
-    // 녹색 1바퀴 회전
-    colorValue = funnyNeo.Color(0, 255, 0, 0);      // Green
-    for(i = 0; i < NUM_LEDS; i++)
-    {
-        funnyNeo.setPixelColor(i, colorValue);
-        funnyNeo.show();
-        delay(50);
-    }
-    // 파랑색 1바퀴 회전
-    colorValue = funnyNeo.Color(0, 0, 255, 0);      // Blue
-    for(i = 0; i < NUM_LEDS; i++)
-    {
-        funnyNeo.setPixelColor(i, colorValue);
-        funnyNeo.show();
-        delay(50);
-    }
-    // 하얀색 1바퀴 회전
-    colorValue = funnyNeo.Color(0, 0, 0, 255);      // White
-    for(i = 0; i < NUM_LEDS; i++)
-    {
-        funnyNeo.setPixelColor(i, colorValue);
-        funnyNeo.show();
-        delay(50);
-    }
-}
+//     //빨간색 1바퀴 회전
+//     colorValue = funnyNeo.Color(255, 0, 0, 0);      // Red
+//     for(i = 0; i < NUM_LEDS; i++)
+//     {
+//         funnyNeo.setPixelColor(i, colorValue);
+//         funnyNeo.show();
+//         delay(50);
+//     }
+//     // 녹색 1바퀴 회전
+//     colorValue = funnyNeo.Color(0, 255, 0, 0);      // Green
+//     for(i = 0; i < NUM_LEDS; i++)
+//     {
+//         funnyNeo.setPixelColor(i, colorValue);
+//         funnyNeo.show();
+//         delay(50);
+//     }
+//     // 파랑색 1바퀴 회전
+//     colorValue = funnyNeo.Color(0, 0, 255, 0);      // Blue
+//     for(i = 0; i < NUM_LEDS; i++)
+//     {
+//         funnyNeo.setPixelColor(i, colorValue);
+//         funnyNeo.show();
+//         delay(50);
+//     }
+//     // 하얀색 1바퀴 회전
+//     colorValue = funnyNeo.Color(0, 0, 0, 255);      // White
+//     for(i = 0; i < NUM_LEDS; i++)
+//     {
+//         funnyNeo.setPixelColor(i, colorValue);
+//         funnyNeo.show();
+//         delay(50);
+//     }
+// }
