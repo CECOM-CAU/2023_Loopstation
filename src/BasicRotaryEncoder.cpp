b01805978d4910c3e1104ad20b01c7d2da4f0d1b
girinssh <tngus5851@naver.com>
2023-09-09 12:51:49 +0900

    Implementing RotaryEncoder with ISR

---
+++ src/BasicRotaryEncoder.cpp
@@ -0,0 +1,21 @@
+#include "BasicRotaryEncoder.h"
+
+int BasicRotaryEncoder::getRotate(){
+    int clk_val = getCLK();
+    int dt_val = getDT();
+    int ret = NONE;
+    if(clk_val != prev_clk_val){
+        Serial.print(clk_val);
+        Serial.print("\t");
+        Serial.println(dt_val);
+
+        if(dt_val != clk_val){
+            ret = CLOCK_WISE;
+        }
+        else{
+            ret = COUNTER_CLOCK_WISE;
+        }
+        prev_clk_val = clk_val;
+    }
+    return ret;
+}
---
+++ src/BasicRotaryEncoder.h
@@ -0,0 +1,34 @@
+#ifndef __BASIC_ROTARY_ENCODER_H__
+#define __BASIC_ROTARY_ENCODER_H__
+
+#include <Arduino.h>
+
+class BasicRotaryEncoder {
+private:
+    const int clk_pin;
+    const int dt_pin;
+    const int sw_pin;
+
+    int prev_clk_val;
+
+    int getDT() {return digitalRead(dt_pin);}
+    int getCLK(){return digitalRead(clk_pin);}
+
+public: 
+    static const int COUNTER_CLOCK_WISE = -1;
+    static const int CLOCK_WISE = 1;
+    static const int NONE = 0;
+
+    BasicRotaryEncoder(int _clk, int _dt, int _sw): clk_pin(_clk), dt_pin(_dt), sw_pin(_sw){
+        pinMode(clk_pin, INPUT);
+        pinMode(dt_pin, INPUT);
+        pinMode(sw_pin, INPUT_PULLUP);
+
+        prev_clk_val = digitalRead(clk_pin);
+
+        attachInterrupt(digitalPinToInterrupt(clk_pin), (void (*))(this->getRotate), CHANGE);
+    }
+
+    void getRotate();
+};
+#endif
